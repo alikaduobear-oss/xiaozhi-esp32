8a6dbf0337089db3f84295319dd7bd169db5f6f5
alikaduobear-oss <alikaduobear@gmail.com>
2026-06-26 17:38:08 +0800

    audio0

--- main/audio/codecs/no_audio_codec.h
+++ main/audio/codecs/no_audio_codec.h
@@ -1,23 +1,25 @@
-#ifndef _NO_AUDIO_CODEC_H
-#define _NO_AUDIO_CODEC_H
-
+#pragma once
 #include "audio_codec.h"
-
-#include <driver/gpio.h>
-#include <driver/i2s_pdm.h>
+#include "driver/i2s_std.h"
+#include "driver/i2s_pdm.h"
+#include "driver/gpio.h"
 #include <mutex>
 
 class NoAudioCodec : public AudioCodec {
-protected:
-    std::mutex data_if_mutex_;
-
+public:
+    virtual ~NoAudioCodec();
     virtual int Write(const int16_t* data, int samples) override;
     virtual int Read(int16_t* dest, int samples) override;
     virtual void EnableInput(bool enable) override;
     virtual void EnableOutput(bool enable) override;
 
-public:
-    virtual ~NoAudioCodec();
+protected:
+    bool duplex_ = false;
+    int input_sample_rate_ = 16000;
+    int output_sample_rate_ = 24000;
+    i2s_chan_handle_t tx_handle_ = nullptr;
+    i2s_chan_handle_t rx_handle_ = nullptr;
+    std::mutex data_if_mutex_;
 };
 
 class NoAudioCodecDuplex : public NoAudioCodec {
@@ -33,9 +35,7 @@ public:
 
 class NoAudioCodecSimplexPdm : public NoAudioCodec {
 public:
-    NoAudioCodecSimplexPdm(int input_sample_rate, int output_sample_rate, gpio_num_t spk_bclk, gpio_num_t spk_ws, gpio_num_t spk_dout, gpio_num_t mic_sck,  gpio_num_t mic_din);
-    NoAudioCodecSimplexPdm(int input_sample_rate, int output_sample_rate, gpio_num_t spk_bclk, gpio_num_t spk_ws, gpio_num_t spk_dout, i2s_std_slot_mask_t spk_slot_mask, gpio_num_t mic_sck,  gpio_num_t mic_din);
-    int Read(int16_t* dest, int samples);
+    NoAudioCodecSimplexPdm(int input_sample_rate, int output_sample_rate, gpio_num_t spk_bclk, gpio_num_t spk_ws, gpio_num_t spk_dout, gpio_num_t mic_sck, gpio_num_t mic_din);
+    NoAudioCodecSimplexPdm(int input_sample_rate, int output_sample_rate, gpio_num_t spk_bclk, gpio_num_t spk_ws, gpio_num_t spk_dout, i2s_std_slot_mask_t spk_slot_mask, gpio_num_t mic_sck, gpio_num_t mic_din);
+    int Read(int16_t* dest, int samples) override;
 };
-
-#endif // _NO_AUDIO_CODEC_H
